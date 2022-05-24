575c2e53ae647f028e2f1b800d482e17e69d7773
jey25 <jjangeoyoung@gmail.com>
2022-05-24 15:34:21 +0900

    1 day

---
+++ python/Quiz.py
@@ -0,0 +1,56 @@
+
+# Quiz_6
+
+# Day 1 - Python Print Function
+# The function is declared like this:
+# print('what to print')
+
+# print("Day 1 - Python Print Function")
+# print("The function is declared like this:")
+# print("print('what to print')")
+
+
+# Quiz_8
+
+# Day 1 - String Manipulation
+# String Concatenation is done with the "+" sign.
+# e.g. print("Hello " + "world")
+# New lines can be created with a backslash and n.
+
+# print("Day 1 - String Manipulation")
+# print('String Concatenation is done with the "+" sign.')
+# print('e.g. print("Hello " + "world")')
+# print("New lines can be created with a backslash and n.")
+
+# Quiz_10
+
+# print(len(input("What is your name? ")))
+
+# Quiz_12
+
+# 🚨 Don't change the code below 👇
+# a = input("a: ")
+# b = input("b: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+# c = a
+# a = b
+# b = c
+#Write your code above this line 👆
+####################################
+
+# 🚨 Don't change the code below 👇
+# print("a: " + a)
+# print("b: " + b)
+
+
+# Quiz_14
+
+# print("Welcome to the Band Name Generator.")
+# city = input("What's name of the city you grew up in?\n")
+# name = input("What is your name?\n")
+
+# print("hello " + name + "." + " I'm from " + city + ".")
+
