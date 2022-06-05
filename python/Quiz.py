f841fb61f400e1fb30d0e103d861c3fd2490b877
jey25 <jjangeoyoung@gmail.com>
2022-06-05 15:31:36 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -476,4 +476,72 @@
 
 # quiz_52
 
+# from re import I
+
+# number = 0
+
+# for i in range(1, 101):
+#   if i % 2 == 0:
+#     number += i
+# print(number)
+
+# quiz_53
+
+# for i in range(1, 101):
+#   if i % 3 == 0 and i % 5 == 0:
+#     print("FizzBuzz!")
+#   elif i % 3 == 0:
+#     print("Fizz")
+#   elif i % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(i)
+
+# quiz_54
+
+#Password Generator Project
+# import random
+# letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+# numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+# symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
+
+# print("Welcome to the PyPassword Generator!")
+# nr_letters= int(input("How many letters would you like in your password?\n")) 
+# nr_symbols = int(input(f"How many symbols would you like?\n"))
+# nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+#Eazy Level - Order not randomised:
+#e.g. 4 letter, 2 symbol, 2 number = JduE&!91
+
+# password = ""
+
+# for i in range(1, nr_letters + 1):
+#   password += random.choice(letters)
+
+# for i in range(1, nr_symbols + 1):
+#   password += random.choice(symbols)
+
+# for i in range(1, nr_numbers + 1):
+#   password += random.choice(numbers)
+
+# list = list(password)
+# random.shuffle(list)
+
+# result = ""
+# for password in list:
+#   result += password
+
+# print(result)
+
+# result = ""
+# for password in password_r:
+#   result += password
+
+# print(result)
+
+# passw = random.shuffle(password)
+
+#Hard Level - Order of characters randomised:
+#e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
+
 
