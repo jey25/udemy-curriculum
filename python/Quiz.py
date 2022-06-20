a4eab0d350802a7e59250c953398ecca91c2b475
jey25 <jjangeoyoung@gmail.com>
2022-06-20 17:34:29 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -869,3 +869,69 @@
 
 #Quiz_83  
 
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+# text = input("Type your message:\n").lower()
+# shift = int(input("Type the shift number:\n"))
+
+#TODO-1: Create a function called 'encrypt' that takes the 'text' and 'shift' as inputs.
+
+# def encrypt(plain_text, shift_amount):
+#   cipher_text = ""
+#   for letter in plain_text:
+#     position = alphabet.index(letter)
+#     new_position = position + shift_amount
+#     new_letter = alphabet[new_position]
+#     cipher_text += new_letter
+#   print(f"The encoded text is {cipher_text}")
+
+    #TODO-2: Inside the 'encrypt' function, shift each letter of the 'text' forwards in the alphabet by the shift amount and print the encrypted text.  
+    #e.g. 
+    #plain_text = "hello"
+    #shift = 5
+    #cipher_text = "mjqqt"
+    #print output: "The encoded text is mjqqt"
+
+    ##HINT: How do you get the index of an item in a list:
+    #https://stackoverflow.com/questions/176918/finding-the-index-of-an-item-in-a-list
+
+    ##🐛Bug alert: What happens if you try to encode the word 'civilization'?🐛
+
+#TODO-3: Call the encrypt function and pass in the user inputs. You should be able to test the code and encrypt a message. 
+# encrypt(plain_text=text, shift_amount=shift)
+
+
+#Quiz_84
+
+# alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+# def caesar(start_text, shift_amount, cipher_direction):
+#   end_text = ""
+#   if cipher_direction == "decode":
+#       shift_amount *= -1
+#   for char in start_text:
+#     if char in alphabet:
+#       position = alphabet.index(char)
+#       new_position = position + shift_amount
+#       end_text += alphabet[new_position]
+#     else:
+#       end_text += char
+#   print(f"Here;s the {direction}d result: {end_text}")
+
+# should_continue = True
+
+# while should_continue:
+#   direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+#   text = input("Type your message:\n").lower()
+#   shift = int(input("Type the shift number:\n"))
+#   # 26보다 큰 수를 입력했을 때 에러 방지 (나눈 후 나머지 값이 shift 값이 되도록 함)
+#   shift = shift % 26
+#   caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+#   result = input("Type yes or no.")
+#   if result == "no":
+#     should_continue = False
+#     print("Good bye")
+
+
