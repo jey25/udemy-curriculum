bb440e8be6dd4642c26676cae140181f0da5d7f8
jey25 <jjangeoyoung@gmail.com>
2022-06-21 11:50:39 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -935,3 +935,33 @@
 #     print("Good bye")
 
 
+# Quiz _ 90
+
+# student_scores = {
+#   "Harry": 81,
+#   "Ron": 78,
+#   "Hermione": 99, 
+#   "Draco": 74,
+#   "Neville": 62,
+# }
+# # 🚨 Don't change the code above 👆
+
+# #TODO-1: Create an empty dictionary called student_grades.
+# student_grade = {}
+
+# #TODO-2: Write your code below to add the grades to student_grades.👇
+# for student in student_scores:
+#   score = student_scores[student]
+#   if score > 90:
+#     student_grade[student] = "수"
+#   elif score > 80:
+#     student_grade[student] = "우"
+#   elif score > 70:
+#     student_grade[student] = "미"
+#   else:
+#     student_grade[student] = "Fail"
+
+# # 🚨 Don't change the code below 👇
+# print(student_grade)
+
+
