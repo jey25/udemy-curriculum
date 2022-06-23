9fe511cc75b8f9443dce5767cf7946120f984af0
jey25 <jjangeoyoung@gmail.com>
2022-06-23 16:57:28 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -995,5 +995,13 @@
 # print(travel_log)
 
 
+# Quiz_92
+# order = {
+#     "starter": {1: "Salad", 2: "Soup"},
+#     "main": {1: ["Burger", "Fries"], 2: ["Steak"]},
+#     "dessert": {1: ["Ice Cream"], 2: []},
+# }
+
+# print(order["main"][2][0])
 
 
