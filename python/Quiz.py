3253e74d1f361f46beeda278ffd8a1451d8eb274
jey25 <jjangeoyoung@gmail.com>
2022-06-23 16:35:53 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -966,30 +966,33 @@
 
 
 # Quiz_92
-travel_log = [
-{
-  "country": "France",
-  "visits": 12,
-  "cities": ["Paris", "Lille", "Dijon"]
-},
-{
-  "country": "Germany",
-  "visits": 5,
-  "cities": ["Berlin", "Hamburg", "Stuttgart"]
-},
-]
-#🚨 Do NOT change the code above
+# travel_log = [
+# {
+#   "country": "France",
+#   "visits": 12,
+#   "cities": ["Paris", "Lille", "Dijon"]
+# },
+# {
+#   "country": "Germany",
+#   "visits": 5,
+#   "cities": ["Berlin", "Hamburg", "Stuttgart"]
+# },
+# ]
+# #🚨 Do NOT change the code above
 
-#TODO: Write the function that will allow new countries
-#to be added to the travel_log. 👇
+# #TODO: Write the function that will allow new countries
+# #to be added to the travel_log. 👇
 
-def add_new_country(country, visits, cities):
+# def add_new_country(country, time, cities):
+#   new_country = {}
+#   new_country["country"] = country
+#   new_country["visits"] = time
+#   new_country["cities"] = cities
+#   travel_log.append(new_country)
   
-
-
-#🚨 Do not change the code below
-add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log)
+# #🚨 Do not change the code below
+# add_new_country("Russia", 2, ["Moscow", "Saint Petersburg"])
+# print(travel_log)
 
 
 
