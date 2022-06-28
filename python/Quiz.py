8ba042cd8c3d7f7e132e14d0348c56a5182343ae
jey25 <jjangeoyoung@gmail.com>
2022-06-28 16:16:56 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -1075,13 +1075,39 @@
 # print(result)
 
 
+#Quiz_101
 
+#Calculator
 
+def add(n1, n2):
+  return n1 + n2
 
+def minus(n1, n2):
+  return n1 - n2
 
+def time(n1, n2):
+  return n1 * n2
 
+def divide(n1, n2):
+  return n1 / n2
 
+operations = {
+  "+":add,
+  "-":minus,
+  "*":time,
+  "/":divide,
+}
 
+num1 = int(input("What is the first number? : "))
+
+for operation in operations:
+  print(operation)
+operation_symbol = input("pick an operation: ")
+num2 = int(input("What is the second number? : "))
+calculation_function = operations[operation_symbol]
+answer = calculation_function(num1, num2)
+
+print(f"{num1} {operation_symbol} {num2} = {answer}")
 
 
 
