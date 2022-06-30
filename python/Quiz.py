73616e5fc544032ae33d0994870e1c927bcf7c7d
jey25 <jjangeoyoung@gmail.com>
2022-06-30 16:35:08 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -1161,15 +1161,27 @@
 
 #Hint 4: Create a deal_card() function that uses the List below to *return* a random card.
 #11 is the Ace.
-#cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+import random
+
+def deal_card():
+  cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
+  card = random.choice(cards)
+  return card
 
 #Hint 5: Deal the user and computer 2 cards each using deal_card() and append().
-#user_cards = []
-#computer_cards = []
 
-#Hint 6: Create a function called calculate_score() that takes a List of cards as input 
-#and returns the score. 
+user_cards = []
+computer_cards = []
+
+for _ in range(2):
+  user_cards.append(deal_card())
+  computer_cards.append(deal_card())
+
+#Hint 6: Create a function called calculate_score() that takes a List of cards as input and returns the score. 
 #Look up the sum() function to help you do this.
+
+def calculate_score():
+  
 
 #Hint 7: Inside calculate_score() check for a blackjack (a hand with only 2 cards: ace + 10) and return 0 instead of the actual score. 0 will represent a blackjack in our game.
 
