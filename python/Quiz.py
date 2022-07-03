f034f122e8b173a40bfcbdd2cce4fc9106e339bc
jey25 <jjangeoyoung@gmail.com>
2022-07-03 23:33:33 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -1191,20 +1191,22 @@ for _ in range(2):
   computer_cards.append(deal_card())
 
 #Hint 9: Call calculate_score(). If the computer or the user has a blackjack (0) or if the user's score is over 21, then the game ends.
-user_score = calculate_score(user_cards)
-computer_score = calculate_score(computer_cards)
 
-print(f"Your card {user_cards}, score {user_score}")
-print(f"computer card {computer_cards[0]}")
+while not is_game_over:
+  user_score = calculate_score(user_cards)
+  computer_score = calculate_score(computer_cards)
 
-if user_score == 0 or computer_score == 0 or user_score > 21:
-  is_game_over = True
-else:
-  user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
-  if user_should_deal == "y":
-    user_cards.append(deal_card())
-  else:
+  print(f"Your card {user_cards}, score {user_score}")
+  print(f"computer card {computer_cards[0]}")
+
+  if user_score == 0 or computer_score == 0 or user_score > 21:
     is_game_over = True
+  else:
+    user_should_deal = input("Type 'y' to get another card, type 'n' to pass: ")
+    if user_should_deal == "y":
+      user_cards.append(deal_card())
+    else:
+      is_game_over = True
 
 #Hint 10: If the game has not ended, ask the user if they want to draw another card. If yes, then use the deal_card() function to add another card to the user_cards List. If no, then the game has ended.
 
