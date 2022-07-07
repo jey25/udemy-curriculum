0403a0654a5fbe98c8d51b29352c9b0bed109e55
jey25 <jjangeoyoung@gmail.com>
2022-07-07 22:26:30 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -1314,10 +1314,10 @@
 # else:
 #   print("You are old.")
 
-# # Fix the Errors
-# age = input("How old are you?")
-# if age > 18:
-# print("You can drive at age {age}.")
+# Fix the Errors
+age = input("How old are you?")
+if age > 18:
+print("You can drive at age {age}.")
 
 # #Print is Your Friend
 # pages = 0
