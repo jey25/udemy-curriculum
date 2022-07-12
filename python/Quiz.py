dd4509b694630d47771562389de9de3ddf7968dd
jey25 <jjangeoyoung@gmail.com>
2022-07-12 16:57:38 +0900

    Update Quiz.py

--- python/Quiz.py
+++ python/Quiz.py
@@ -1348,7 +1348,34 @@
 #   print("This is an odd number.")
   
 
+#Quiz_ 129
 
+# year = int(input("Which year do you want to check?"))
+
+# if year % 4 == 0:
+#   if year % 100 == 0:
+#     if year % 400 == 0:
+#       print("Leap year.")
+#     else:
+#       print("Not leap year.")
+#   else:
+#     print("Leap year.")
+# else:
+#   print("Not leap year.")
+  
+
+
+# Quiz_130
+
+# for number in range(1, 101):
+#   if number % 3 == 0 and number % 5 == 0:
+#     print("FizzBuzz")
+#   elif number % 3 == 0:
+#     print("Fizz")
+#   elif number % 5 == 0:
+#     print("Buzz")
+#   else:
+#     print(number)
 
 
 
