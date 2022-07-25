33e75f006666096ed64c21ed4043af79febe330c
jey25 <jjangeoyoung@gmail.com>
2022-07-25 14:23:38 +0900

    class

---
+++ python/145_venv/main.py
@@ -0,0 +1,16 @@
+from tkinter import Variable
+import module
+
+print(module.variable)
+
+from turtle import Screen, Turtle
+timmy = Turtle()
+
+timmy.shape("turtle")
+timmy.color("coral")
+timmy.forward(100)
+
+my_screen = Screen()
+print(my_screen.canvheight)
+
+my_screen.exitonclick()
