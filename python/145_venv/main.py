fe562206e5f01c930206deb0d79a7d6323114b53
jey25 <jjangeoyoung@gmail.com>
2022-07-25 15:35:23 +0900

    Update main.py

--- python/145_venv/main.py
+++ python/145_venv/main.py
@@ -15,5 +15,12 @@
 
 # my_screen.exitonclick()
 
-import prettytable
+from prettytable import PrettyTable
+
+table = PrettyTable()
+
+table.add_column("Pokemon Name", ["Pikachu", "Squirtle", "Charamander"])
+table.add_column("Type", ["Electric", "Water", "Fire"])
+
+print(table)
 
