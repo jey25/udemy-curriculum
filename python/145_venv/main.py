8ffb8dfafc180ea653f46b3d97190cefeeb80f6c
jey25 <jjangeoyoung@gmail.com>
2022-07-25 15:15:36 +0900

    Update main.py

--- python/145_venv/main.py
+++ python/145_venv/main.py
@@ -1,16 +1,19 @@
-from tkinter import Variable
-import module
+# from tkinter import Variable
+# import module
 
-print(module.variable)
+# print(module.variable)
 
-from turtle import Screen, Turtle
-timmy = Turtle()
+# from turtle import Screen, Turtle
+# timmy = Turtle()
 
-timmy.shape("turtle")
-timmy.color("coral")
-timmy.forward(100)
+# timmy.shape("turtle")
+# timmy.color("coral")
+# timmy.forward(100)
 
-my_screen = Screen()
-print(my_screen.canvheight)
+# my_screen = Screen()
+# print(my_screen.canvheight)
 
-my_screen.exitonclick()
+# my_screen.exitonclick()
+
+import prettytable
+
