99135c90bea82097334a39a87d2a3409cacfd1b6
jey25 <jjangeoyoung@gmail.com>
2022-08-01 16:09:12 +0900

    make report

--- python/oop-coffee-machine-start/main.py
+++ python/oop-coffee-machine-start/main.py
@@ -2,3 +2,8 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+money_machine = MoneyMachine()
+coffee_maker = CoffeeMaker()
+
+money_machine.report()
+coffee_maker.report()
