4a7b1a8e7faef4932fa0a0e40150fb639ae1d4e3
jey25 <jjangeoyoung@gmail.com>
2022-08-04 17:41:48 +0900

    Update main.py

--- python/152_start/main.py
+++ python/152_start/main.py
@@ -3,10 +3,12 @@ class User:
   def __init__(self, user_id, username):
     self.id = user_id
     self.username = username
+    self.followers = 0
 
 user1 = User("001", "jang")
 user2 = User("002", "jack")
 
 print(user1.username)
 print(user2.id)
+print(user2.followers)
 
