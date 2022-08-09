57fb5ac3f4200f30f8efeae8ef65b41d97b0cd70
jey25 <jjangeoyoung@gmail.com>
2022-08-09 16:28:17 +0900

    quiz_brain

--- python/quiz-game-start/main.py
+++ python/quiz-game-start/main.py
@@ -6,5 +6,5 @@ question_bank = []
 for question in question_data:
   question_text = question["text"]
   question_answer = question["answer"]
-  new_question = Question(q_text=question_text, q_answer=question_answer)
+  new_question = Question(question_text, question_answer)
   question_bank.append(new_question)
---
+++ python/quiz-game-start/quiz_brain.py
@@ -0,0 +1,9 @@
+from re import L
+
+
+class QuizBrain:
+
+  def __init__(self):
+    self.question_number = 0
+    self.question_list = q_list
+
