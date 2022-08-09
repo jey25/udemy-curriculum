b5a04ffc68441b356b7bc06a7a5befc8019d511a
jey25 <jjangeoyoung@gmail.com>
2022-08-09 15:56:22 +0900

    Update main.py

--- python/quiz-game-start/main.py
+++ python/quiz-game-start/main.py
@@ -8,5 +8,3 @@ for question in question_data:
   question_answer = question["answer"]
   new_question = Question(q_text=question_text, q_answer=question_answer)
   question_bank.append(new_question)
-
-print(question_bank)
