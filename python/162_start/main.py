5bd87658c27ae2a6888edf373b0cd8952014d785
jey25 <jjangeoyoung@gmail.com>
2022-08-13 00:51:32 +0900

    Create main.py

---
+++ python/162_start/main.py
@@ -0,0 +1,22 @@
+from turtle import Screen, Turtle
+import turtle
+
+t = Turtle()
+
+t.shape("turtle")
+t.color("red")
+t.forward(100)
+t.right(90)
+
+
+
+
+
+
+
+
+
+
+
+screen = Screen()
+screen.exitonclick()
