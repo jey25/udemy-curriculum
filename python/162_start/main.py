f6cfd19ff85f8f4e8038e0040218b581e1b2e70e
jey25 <jjangeoyoung@gmail.com>
2022-08-17 16:35:17 +0900

    Update main.py

--- python/162_start/main.py
+++ python/162_start/main.py
@@ -5,13 +5,11 @@ t = Turtle()
 
 t.shape("turtle")
 t.color("red")
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
-t.right(90)
-t.forward(100)
+
+for _ in range(4):
+  t.forward(100)
+  t.right(90) 
+
 
 
 
