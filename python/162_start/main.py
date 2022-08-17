24fdb0b52b9274d524b2f6ca4a505480bbfa094a
jey25 <jjangeoyoung@gmail.com>
2022-08-17 16:52:41 +0900

    Update main.py

--- python/162_start/main.py
+++ python/162_start/main.py
@@ -7,9 +7,16 @@ t = Turtle()
 t.shape("turtle")
 t.color("red")
 
-for _ in range(4):
-  t.forward(100)
-  t.right(90) 
+# for _ in range(4):
+#   t.forward(100)
+  # t.right(90) 
+
+for _ in range(15):
+  t.forward(5)
+  t.up()
+  t.forward(5)
+  t.down()
+
 
 
 import heroes
