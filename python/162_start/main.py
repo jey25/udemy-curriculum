e8a6f243491c2361b6008d11ed6d71ea77529dd1
jey25 <jjangeoyoung@gmail.com>
2022-08-21 21:40:53 +0900

    Update main.py

--- python/162_start/main.py
+++ python/162_start/main.py
@@ -17,22 +17,33 @@ t.color("red")
 #   t.forward(5)
 #   t.down()
 
-t.forward(100)
-t.rt(120)
-t.forward(100)
-t.rt(120)
-t.forward(100)
-t.rt(120)
-t.forward(100)
-t.rt(90)
-t.forward(100)
-t.rt(90)
-t.forward(100)
-t.rt(90)
-t.forward(100)
-t.rt(90)
-t.forward(100)
+for _ in range(3):
+  t.forward(100)
+  t.rt(360/3)
 
+t.color("blue")
+
+for _ in range(4):
+  t.forward(100)
+  t.rt(360/4)
+
+t.color("purple")
+
+for _ in range(5):
+  t.forward(100)
+  t.rt(360/5)
+
+t.color("black")
+
+for _ in range(6):
+  t.forward(100)
+  t.rt(360/6)
+
+t.color("gray")
+
+for _ in range(7):
+  t.forward(100)
+  t.rt(360/7)
 
 
 
