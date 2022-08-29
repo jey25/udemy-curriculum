c566853713428a4e40cc77117c0f0e1410f48281
jey25 <jjangeoyoung@gmail.com>
2022-08-29 15:17:24 +0900

    Update main.py

---
+++ python/hirst_painting/main.py
@@ -0,0 +1,15 @@
+# import colorgram
+
+# rgb_colors = []
+
+# # Extract 6 colors from an image.
+# colors = colorgram.extract('image.jpg', 6)
+
+# for color in colors:
+#   r = color.rgb.r
+#   g = color.rgb.g
+#   b = color.rgb.b
+#   new_color = (r,g,b)
+#   rgb_colors.append(new_color)
+
+# print(rgb_colors)
