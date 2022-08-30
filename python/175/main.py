2a0bed1e9fd21c912e2defec4c20ab15d1a5afd0
jey25 <jjangeoyoung@gmail.com>
2022-08-30 12:57:17 +0900

    Update main.py

--- python/175/main.py
+++ python/175/main.py
@@ -3,5 +3,12 @@ from turtle import Turtle, Screen
 tim = Turtle()
 screen = Screen()
 
+def move_forwords():
+  tim.forward(10)
+
+
+
 
 screen.listen()
+screen.onkey(key="space", fun=move_forwords)
+screen.exitonclick()
