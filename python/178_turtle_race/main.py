17ed657f2bfde698c4d72df782e7ce79e629361f
jey25 <jjangeoyoung@gmail.com>
2022-08-31 20:31:59 +0900

    Update main.py

--- python/178_turtle_race/main.py
+++ python/178_turtle_race/main.py
@@ -5,12 +5,15 @@ from turtle import Turtle, Screen
 screen = Screen()
 screen.setup(width=500, height=400)
 user_bet = screen.textinput(title="Make your bet", prompt="Which turrle will win the race? Enter a color : ")
+colors = ["red", "orange", "yellow", "blue", "green", "purple"]
+y_positions = [-70, -40, -10, 20, 50, 80]
 
 
-
-tim = Turtle(shape="turtle")
-tim.penup()
-tim.goto(x=-230, y=-100)
+for turtle_index in range(0, 6):
+    tim = Turtle(shape="turtle")
+    tim.penup()
+    tim.color(colors[turtle_index])
+    tim.goto(x=-230, y=y_positions[turtle_index])
 
 
 
