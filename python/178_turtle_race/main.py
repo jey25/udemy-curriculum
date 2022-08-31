a684457247be515db3542b3ea7ef5cf33fe6e27a
jey25 <jjangeoyoung@gmail.com>
2022-08-31 20:23:36 +0900

    Update main.py

--- python/178_turtle_race/main.py
+++ python/178_turtle_race/main.py
@@ -1,7 +1,17 @@
 from turtle import Turtle, Screen
 
-tim = Turtle()
+
+
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turrle will win the race? Enter a color : ")
+
+
+
+tim = Turtle(shape="turtle")
+tim.penup()
+tim.goto(x=-230, y=-100)
+
 
 
 screen.exitonclick()
