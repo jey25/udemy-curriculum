8b2062a659dee2c9b8c996b4450d190bbf3ed662
jey25 <jjangeoyoung@gmail.com>
2023-01-17 23:22:17 +0900

    Update main.py

--- python2/main.py
+++ python2/main.py
@@ -1,8 +1,4 @@
-a = input("a:")
-b = input("b:")
-
-
-
-print(a)
-print(b)
-
+print("Welcome to the band name Generator.")
+city_name = input("What's name of the city you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+print("Your band name could be " + city_name + " " + pet_name)
